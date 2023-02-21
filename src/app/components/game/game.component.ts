b3088e6bddc096d2b6180f2337b32bdfd8382b42
Ryan Kline <kliner@mail.gvsu.edu>
2023-02-20 21:28:05 -0500

    Add win condition

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -12,12 +12,120 @@ export class GameComponent {
   player1 = new player(1, 'red');
   player2 = new player(2, 'yellow');
   currentPlayer = this.player1
+  winner : number = 0
+  message = ""
+  gameOver = false
 
   rows : number[] = [0,1,2,3,4,5]
   cols : number[] = [0,1,2,3,4,5,6]
   cellColor : string = "lightgrey"
   cells = this.createGrid(this.rows.length, this.cols.length, this.cellColor)
 
+
+  fourInARow(tokens : string[], tokenColor : string) : boolean{
+    // console.log(tokens)
+    let count = 0
+
+    for(let i=0; i < tokens.length; i++){
+      if(tokens[i] == tokenColor){
+        count++
+      }else{
+        count = 0
+      }
+
+      if(count == 4){
+        return true
+      }
+    }
+    return false
+  }
+
+  checkForWin(row : number, col : number, tokenColor : string){
+    let tokensInRow = this.getTokensAtRow(row)
+    let tokensInCol = this.getTokensAtCol(col)
+    let tokensInAscendingDiag = this.getAscendingDiagAt(row, col)
+    let tokensInDescendingDiag = this.getDescendingDiagAt(row, col)
+
+    let tokenArrays = [tokensInRow, tokensInCol, tokensInAscendingDiag, tokensInDescendingDiag]
+
+    console.log("Check for win:", tokenArrays[2])
+    console.log(row, col)
+    for(let i = 0; i < tokenArrays.length; i++){
+    
+      if(this.fourInARow(tokenArrays[i], tokenColor) == true){
+        this.winner = this.currentPlayer.getPlayerNumber()
+        this.endGame()
+        
+      }
+    }
+  }
+
+  getTokensAtRow(rowNum : number) : string[]{
+    let tokens : string[] = []
+
+    for(let i = 0; i < this.cols.length; i++){
+      tokens.push(this.cells[rowNum][i].getColor())
+    }
+
+    return tokens
+  }
+
+  getTokensAtCol(colNum : number) : string[]{
+    let tokens : string[] = []
+
+    for(let i = 0; i < this.rows.length; i++){
+      tokens.push(this.cells[i][colNum].getColor())
+    }
+
+    return tokens
+  }
+
+  getAscendingDiagAt(rowNum : number, colNum : number){
+
+        let tokens : string[] = [];
+        let start_row = rowNum + colNum;
+        let start_col = 0;
+
+        if(start_row > (this.rows.length -1)){
+            let diff = start_row - (this.rows.length -1);
+            start_row -= diff;
+            start_col += diff;
+        }
+
+        let next_row = start_row;
+        let next_col = start_col;
+
+        while(next_row > -1 && next_col < this.cols.length){
+            tokens.push(this.cells[next_row][next_col].getColor());
+            next_row--;
+            next_col++;
+        }
+        return tokens; 
+
+  }
+
+  getDescendingDiagAt(rowNum : number, colNum : number) :string[]{
+    let tokens :string[] = [];
+    let start_row = rowNum - colNum;
+    let start_col = 0;
+
+    if(start_row < 0){
+        let diff = 0 - start_row;
+        start_row += diff;
+        start_col += diff;
+    }
+
+    let next_row = start_row;
+    let next_col = start_col;
+
+    while(next_row < this.rows.length && next_col < this.cols.length){
+        tokens.push(this.cells[next_row][next_col].getColor());
+        next_row++;
+        next_col++;
+    }
+    return tokens;
+    
+}
 
   dropToken(columnNumber : number){
     let topCell = this.cells[0][columnNumber]
@@ -27,17 +135,26 @@ export class GameComponent {
         let currentCell = this.cells[i][columnNumber]
 
         while(currentCell.isEmpty == false){
-          currentCell = this.cells[i][columnNumber]
           i--
+          currentCell = this.cells[i][columnNumber]
+          
         }
         currentCell.setColor(this.currentPlayer.getTokenColor())
         currentCell.isEmpty = false
 
-    }else{
-      console.log("Column full.")
-    }
-    
+        this.checkForWin(i,columnNumber, this.currentPlayer.getTokenColor())
+        this.endTurn()
 
+        if(!this.gameOver){
+          this.message = ""
+        }
+        
+    }else{
+      this.message = `Column ${columnNumber} is full. Try again.`
+    }
+  }
+
+  endTurn(){
     if(this.currentPlayer.getPlayerNumber() == 1){
       this.currentPlayer = this.player2
     }else{
@@ -58,5 +175,11 @@ export class GameComponent {
     }
     return grid
   }
+
+  endGame(){
+    this.gameOver = true
+    this.message = `Player ${this.winner} Won!`
+  }
+
 
 }
